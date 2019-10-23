411ca18ccbd8fad290a60259cd5673a5bec5a187
Neil Rhodes <neil@pobox.com>
2019-10-23 10:32:48 -0700

    Add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='keras_one_cycle_clr',
+      version='0.1',
+      description='One cycle learning for Keras',
+      url='http://github.com/nrhodes/keras_one_cycle_clr',
+      author='psklight',
+      author_email='neil@pobox.com',
+      license='MIT',
+      packages=['keras_one_cycle_clr'],
+      zip_safe=False)
+
